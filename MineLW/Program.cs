fd68c8906ab988dad0322fac729d51fddf52f91a
TheWhoosher <18100602+TheWhoosher@users.noreply.github.com>
2019-10-02 23:05:33 +0200

    Implemented server bootstrap

--- MineLW/Program.cs
+++ MineLW/Program.cs
@@ -1,12 +1,27 @@
-﻿﻿using System;
+﻿using System;
+using System.Threading;
 
 namespace MineLW
 {
     internal static class Program
     {
+        private static GameServer _server;
+
         private static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.Title = GameServer.Name;
+            Thread.CurrentThread.Name = "Main";
+
+            Console.CancelKeyPress += HandleCancelKeyPressed;
+
+            _server = new GameServer();
+            _server.Start();
+        }
+
+        private static void HandleCancelKeyPressed(object sender, ConsoleCancelEventArgs e)
+        {
+            e.Cancel = true;
+            _server?.Shutdown();
         }
     }
 }
