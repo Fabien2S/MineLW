321b6215ac42386338365dc165700a36141d2367
TheWhoosher <18100602+TheWhoosher@users.noreply.github.com>
2019-10-07 18:40:20 +0200

    Created Identifier struct

---
+++ MineLW.API/Utils/Identifier.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace MineLW.API.Utils
+{
+    public struct Identifier
+    {
+        private readonly string _namespace;
+        private readonly string _key;
+
+        [NonSerialized] private readonly int _hash;
+
+        public Identifier(string @namespace, string key)
+        {
+            _namespace = @namespace;
+            _key = key;
+            _hash = (@namespace, key).GetHashCode();
+        }
+
+        public override string ToString()
+        {
+            return _namespace + ':' + _key;
+        }
+
+        public override int GetHashCode()
+        {
+            return _hash;
+        }
+
+        public bool Equals(Identifier other)
+        {
+            return _hash == other._hash;
+        }
+
+        public override bool Equals(object obj)
+        {
+            return obj is Identifier other && Equals(other);
+        }
+    }
+}
