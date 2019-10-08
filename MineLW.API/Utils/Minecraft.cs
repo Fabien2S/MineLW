094924997ea6c4edfc74ae6cc585739fb76f7cf4
TheWhoosher <18100602+TheWhoosher@users.noreply.github.com>
2019-10-08 22:14:50 +0200

    Added Minecraft util class

---
+++ MineLW.API/Utils/Minecraft.cs
@@ -0,0 +1,25 @@
+﻿namespace MineLW.API.Utils
+{
+    public static class Minecraft
+    {
+        private const string Namespace = "minecraft";
+
+        public static class Units
+        {
+            public static float ToMeterPerSeconds(float minecraftMetric)
+            {
+                return (float) (43.178 * minecraftMetric - 0.02141);
+            }
+        }
+
+        public static class Channels
+        {
+            public static readonly Identifier Brand = new Identifier(Namespace, "brand");
+        }
+
+        public static Identifier CreateKey(string key)
+        {
+            return new Identifier(Namespace, key);
+        }
+    }
+}
