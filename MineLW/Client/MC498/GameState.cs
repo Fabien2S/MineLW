830cf04a272c71e38cf7545c6b6f2d44a8270411
TheWhoosher <18100602+TheWhoosher@users.noreply.github.com>
2019-10-08 08:00:36 +0200

    Added MC498 support for testing purposes

---
+++ MineLW/Client/MC498/GameState.cs
@@ -0,0 +1,93 @@
+﻿using MineLW.API.Client;
+using MineLW.API.Text;
+using MineLW.Client.MC498.Client;
+using MineLW.Networking;
+using MineLW.Networking.Messages;
+using MineLW.Networking.Messages.Serialization;
+
+namespace MineLW.Client.MC498
+{
+    public class GameState : Networking.States.Game.GameState
+    {
+        public override IMessage CreateDisconnectMessage(TextComponent reason)
+        {
+            return null;
+        }
+
+        protected override IMessageSerializer[] GetSerializers()
+        {
+            return new IMessageSerializer[]
+            {
+                null,
+                null,
+                null,
+                null,
+                null,
+                null,
+                null,
+                null,
+                null,
+                null,
+                null,
+                null,
+                null,
+                null,
+                new MessageClientChatMessage(),
+                null,
+                null,
+                null,
+                null,
+                null,
+                null,
+                null,
+                null,
+                null,
+                null,
+                null,
+                new MessageClientDisconnect(),
+                null,
+                null,
+                null,
+                null,
+                null,
+                null,
+                null,
+                null,
+                null,
+                null,
+                new MessageClientInitGame(),
+                null,
+                null,
+                null,
+                null,
+                null,
+                null,
+                null,
+                null,
+                null,
+                null,
+                null,
+                null,
+                null,
+                null,
+                null,
+                new MessageClientPlayerTeleport()
+            };
+        }
+
+        protected override IMessageDeserializer[] GetDeserializers()
+        {
+            return new IMessageDeserializer[0];
+        }
+
+        protected override MessageController CreateController(NetworkClient client)
+        {
+            return new GameController(client);
+        }
+        
+        public override IClient CreateClient(NetworkClient networkClient)
+        {
+            return new GameClient(networkClient);
+        }
+    }
+}
