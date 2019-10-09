6d65463053f59ac5f8662490980ce522d46f2d80
TheWhoosher <18100602+TheWhoosher@users.noreply.github.com>
2019-10-09 23:12:27 +0200

    Added base Plugin API

---
+++ MineLW.API/Plugin/IPluginManager.cs
@@ -0,0 +1,12 @@
+﻿namespace MineLW.API.Plugin
+{
+    public interface IPluginManager
+    {
+        void LoadPlugin(IPlugin plugin);
+        void UnloadPlugin(IPlugin plugin);
+
+        void EnablePlugin(IPlugin plugin);
+        void DisablePlugin(IPlugin plugin);
+        void ReloadPlugin(IPlugin plugin);
+    }
+}
