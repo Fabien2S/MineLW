59d0d58a3d8bae78890ca40dff42c9fdde50f53b
TheWhoosher <18100602+TheWhoosher@users.noreply.github.com>
2019-10-09 15:21:20 +0200

    Added Init method

--- MineLW.API/Client/IClient.cs
+++ MineLW.API/Client/IClient.cs
@@ -1,4 +1,5 @@
-﻿using MineLW.API.Text;
+﻿using MineLW.API.Entities.Living.Player;
+using MineLW.API.Text;
 using MineLW.API.Utils;
 
 namespace MineLW.API.Client
@@ -6,7 +7,8 @@ namespace MineLW.API.Client
     public interface IClient
     {
         PlayerProfile Profile { get; }
-        
+
+        void Init(IEntityPlayer player);
         void SendMessage(TextComponent message);
         void Kick(TextComponentString reason);
     }
