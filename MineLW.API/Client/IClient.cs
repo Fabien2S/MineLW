1ee4a20395795ba2dbd2d29d5bcf5471aca51e7e
TheWhoosher <18100602+TheWhoosher@users.noreply.github.com>
2019-10-09 15:17:47 +0200

    Added Profile field in IClient

--- MineLW.API/Client/IClient.cs
+++ MineLW.API/Client/IClient.cs
@@ -1,9 +1,12 @@
 ﻿using MineLW.API.Text;
+using MineLW.API.Utils;
 
 namespace MineLW.API.Client
 {
     public interface IClient
     {
+        PlayerProfile Profile { get; }
+        
         void SendMessage(TextComponent message);
         void Kick(TextComponentString reason);
     }
