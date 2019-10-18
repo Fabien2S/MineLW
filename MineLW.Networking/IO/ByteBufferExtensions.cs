6bcc69c9aa4bffa0b7ca94ec264dc6ee49498cf1
TheWhoosher <18100602+TheWhoosher@users.noreply.github.com>
2019-10-18 14:25:51 +0200

    Code cleanup

--- MineLW.Networking/IO/ByteBufferExtensions.cs
+++ MineLW.Networking/IO/ByteBufferExtensions.cs
@@ -5,7 +5,6 @@ using DotNetty.Buffers;
 using MineLW.API.Math;
 using MineLW.API.Utils;
 using MineLW.API.Worlds.Chunks;
-using MineLW.Serialization;
 using MineLW.Serialization.NBT;
 using Newtonsoft.Json;
 
@@ -36,10 +35,11 @@ namespace MineLW.Networking.IO
             return bytes;
         }
 
-        public static void WriteByteArray(this IByteBuffer buffer, byte[] bytes)
+        public static IByteBuffer WriteByteArray(this IByteBuffer buffer, byte[] bytes)
         {
             buffer.WriteVarInt32(bytes.Length);
             buffer.WriteBytes(bytes);
+            return buffer;
         }
 
         public static string ReadUtf8(this IByteBuffer buffer, short maxLen = short.MaxValue)
@@ -61,7 +61,7 @@ namespace MineLW.Networking.IO
             }
         }
 
-        public static void WriteUtf8(this IByteBuffer buffer, string s)
+        public static IByteBuffer WriteUtf8(this IByteBuffer buffer, string s)
         {
             if (s.Length > short.MaxValue)
                 throw new IndexOutOfRangeException("String is too long");
@@ -69,11 +69,13 @@ namespace MineLW.Networking.IO
             var bytes = Encoding.UTF8.GetBytes(s);
             buffer.WriteVarInt32(bytes.Length);
             buffer.WriteBytes(bytes);
+            return buffer;
         }
 
-        public static void WriteIdentifier(this IByteBuffer buffer, Identifier identifier)
+        public static IByteBuffer WriteIdentifier(this IByteBuffer buffer, Identifier identifier)
         {
             buffer.WriteUtf8(identifier.ToString());
+            return buffer;
         }
 
         public static T ReadJson<T>(this IByteBuffer buffer)
@@ -82,49 +84,56 @@ namespace MineLW.Networking.IO
             return JsonConvert.DeserializeObject<T>(serialized);
         }
 
-        public static void WriteJson(this IByteBuffer buffer, object @object)
+        public static IByteBuffer WriteJson(this IByteBuffer buffer, object @object)
         {
             var serialized = JsonConvert.SerializeObject(@object);
             buffer.WriteUtf8(serialized);
+            return buffer;
         }
 
-        public static void WriteVector3F(this IByteBuffer buffer, Vector3 vector3)
+        public static IByteBuffer WriteVector3F(this IByteBuffer buffer, Vector3 vector3)
         {
             buffer.WriteFloat(vector3.X);
             buffer.WriteFloat(vector3.Y);
             buffer.WriteFloat(vector3.Z);
+            return buffer;
         }
 
-        public static void WriteVector3D(this IByteBuffer buffer, Vector3 vector3)
+        public static IByteBuffer WriteVector3D(this IByteBuffer buffer, Vector3 vector3)
         {
             buffer.WriteDouble(vector3.X);
             buffer.WriteDouble(vector3.Y);
             buffer.WriteDouble(vector3.Z);
+            return buffer;
         }
 
-        public static void WriteVector2F(this IByteBuffer buffer, Vector2 vector3)
+        public static IByteBuffer WriteVector2F(this IByteBuffer buffer, Vector2 vector3)
         {
             buffer.WriteFloat(vector3.X);
             buffer.WriteFloat(vector3.Y);
+            return buffer;
         }
 
-        public static void WriteRotation(this IByteBuffer buffer, Rotation rotation)
+        public static IByteBuffer WriteRotation(this IByteBuffer buffer, Rotation rotation)
         {
             buffer.WriteFloat(rotation.Yaw);
             buffer.WriteFloat(rotation.Pitch);
+            return buffer;
         }
 
-        public static void WriteChunkPosition(this IByteBuffer buffer, ChunkPosition position)
+        public static IByteBuffer WriteChunkPosition(this IByteBuffer buffer, ChunkPosition position)
         {
             buffer.WriteInt(position.X);
             buffer.WriteInt(position.Z);
+            return buffer;
         }
 
-        public static void WriteCompound(this IByteBuffer buffer, NbtCompound compound)
+        public static IByteBuffer WriteCompound(this IByteBuffer buffer, NbtCompound compound)
         {
             buffer.WriteByte(compound.Id);
             buffer.WriteShort(0); // empty name
             compound.Serialize(buffer);
+            return buffer;
         }
     }
 }
