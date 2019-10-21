8244af0942d4005c56634e793eab371d986278a8
TheWhoosher <18100602+TheWhoosher@users.noreply.github.com>
2019-10-21 21:57:39 +0200

    Added WorldContext documentation

--- MineLW.API/Worlds/IWorldContext.cs
+++ MineLW.API/Worlds/IWorldContext.cs
@@ -10,10 +10,16 @@ namespace MineLW.API.Worlds
         IWorld World { get; }
         IChunkManager ChunkManager { get; }
         IEntityManager EntityManager { get; }
-        
+
+        /// <summary>
+        /// Gets the block at the current position, or null
+        /// </summary>
+        /// <param name="position">the position when the block is located</param>
+        /// <returns>the block, or null if empty</returns>
         IBlockState GetBlock(Vector3Int position);
+
         void SetBlock(Vector3Int position, IBlockState blockState);
-        
+
         T GetOptionRaw<T>(WorldOption<T> option);
         T GetOption<T>(WorldOption<T> option);
         void SetOption<T>(WorldOption<T> option, T value);
