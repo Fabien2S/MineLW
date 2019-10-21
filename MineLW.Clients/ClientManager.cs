49d7b8ac721d47cae30d63ff1360104c236bdd3b
TheWhoosher <18100602+TheWhoosher@users.noreply.github.com>
2019-10-21 22:33:11 +0200

    Moved World set-up into the GameServer instead of here. This help reduces dependencies across projects

--- MineLW.Clients/ClientManager.cs
+++ MineLW.Clients/ClientManager.cs
@@ -1,11 +1,9 @@
 using System.Collections.Generic;
-using System.Numerics;
 using MineLW.API;
 using MineLW.API.Client;
 using MineLW.API.Entities.Living.Player;
 using MineLW.API.Worlds;
 using MineLW.Entities.Living.Player;
-using MineLW.Worlds.Chunks.Generator;
 
 namespace MineLW.Clients
 {
@@ -23,21 +21,15 @@ namespace MineLW.Clients
         public void Initialize(IClientConnection connection, PlayerProfile profile)
         {
             var client = new Client(connection, profile);
-            
-            var player = new EntityPlayer(0, client);
 
             var worldManager = _server.WorldManager;
-            var defaultWorld = worldManager.CreateWorld(worldManager.DefaultWorld);
-
-            var blockManager = _server.BlockManager;
-            var blockState = blockManager.CreateState(Minecraft.Blocks.Stone);
-            
-            defaultWorld.ChunkManager.Generator = new DefaultChunkGenerator(blockState);
-            defaultWorld.SetOption(WorldOption.SpawnPosition, Vector3.UnitY);
-            
-            player.WorldContext = defaultWorld;
-            player.Position = defaultWorld.GetOption(WorldOption.SpawnPosition);
-            player.Rotation = defaultWorld.GetOption(WorldOption.SpawnRotation);
+            var defaultWorld = worldManager[worldManager.DefaultWorld];
+            var player = new EntityPlayer(0, client)
+            {
+                WorldContext = defaultWorld,
+                Position = defaultWorld.GetOption(WorldOption.SpawnPosition),
+                Rotation = defaultWorld.GetOption(WorldOption.SpawnRotation)
+            };
 
             client.Init(player);
             _clients.Add(client);
