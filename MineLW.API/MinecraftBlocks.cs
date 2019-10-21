1229979e03735d7d030d87840ca8f5f242e318b4
TheWhoosher <18100602+TheWhoosher@users.noreply.github.com>
2019-10-21 21:55:43 +0200

    Added Minecraft blocks

---
+++ MineLW.API/MinecraftBlocks.cs
@@ -0,0 +1,13 @@
+﻿using MineLW.API.Utils;
+
+namespace MineLW.API
+{
+    public static partial class Minecraft
+    {
+        public static class Blocks
+        {
+            public static readonly Identifier Air = CreateIdentifier("air");
+            public static readonly Identifier Stone = CreateIdentifier("stone");
+        }
+    }
+}
