31f30784bb5c587d230a0341f809f96ff3370c2e
TheWhoosher <18100602+TheWhoosher@users.noreply.github.com>
2019-10-22 14:32:49 +0200

    Fixed compression not working

--- MineLW.Networking/Handlers/CompressionHandler.cs
+++ MineLW.Networking/Handlers/CompressionHandler.cs
@@ -2,8 +2,9 @@ using System.Collections.Generic;
 using DotNetty.Buffers;
 using DotNetty.Codecs;
 using DotNetty.Transport.Channels;
-using Ionic.Zlib;
+using ICSharpCode.SharpZipLib.Zip.Compression;
 using MineLW.Networking.IO;
+using NLog;
 
 namespace MineLW.Networking.Handlers
 {
@@ -11,48 +12,50 @@ namespace MineLW.Networking.Handlers
     {
         public const string Name = "compression";
         
-        private readonly ZlibCodec _compressCodec;
-        private readonly ZlibCodec _decompressCodec;
+        private static readonly Logger Logger = LogManager.GetCurrentClassLogger();
+        
+        private static readonly byte[] Buffer = new byte[8192];
+        
+        private readonly Deflater _deflater;
+        private readonly Inflater _inflater;
 
         private readonly int _compressionThreshold;
 
         public CompressionHandler(int compressionThreshold)
         {
-            _compressCodec= new ZlibCodec(CompressionMode.Compress);
-            _decompressCodec= new ZlibCodec(CompressionMode.Decompress);
+            _deflater= new Deflater(Deflater.DEFAULT_COMPRESSION);
+            _inflater= new Inflater();
             
             _compressionThreshold = compressionThreshold;
         }
 
         protected override void Encode(IChannelHandlerContext ctx, IByteBuffer msg, List<object> output)
         {
-            var data = msg.ToArray(out var offset, out var length);
-            var buffer = ctx.Allocator.Buffer(length);
-
+            var buffer = Unpooled.Buffer();
+            
             if (msg.ReadableBytes >= _compressionThreshold)
             {
-                _compressCodec.InputBuffer = data;
-                _compressCodec.NextIn = offset;
-                _compressCodec.AvailableBytesIn = length;
+                var data = msg.ToArray(out var offset, out var count);
 
-                var compressedData = new byte[length];
-                _compressCodec.OutputBuffer = compressedData;
-                _compressCodec.NextOut = 0;
-                _compressCodec.AvailableBytesOut = length;
+                buffer.WriteVarInt32(count);
                 
-                _compressCodec.Deflate(FlushType.Finish);
-                _compressCodec.EndDeflate();
-                _compressCodec.ResetDeflate();
+                _deflater.SetInput(data, offset, count);
+                _deflater.Finish();
                 
-                buffer.WriteVarInt32(length);
-                buffer.WriteBytes(compressedData);
+                while (!_deflater.IsFinished)
+                {
+                    var read = _deflater.Deflate(Buffer);
+                    buffer.WriteBytes(Buffer, 0, read);
+                }
+                
+                _deflater.Reset();
             }
             else
             {
                 buffer.WriteVarInt32(0);
-                buffer.WriteBytes(data, offset, length);
+                buffer.WriteBytes(msg);
             }
-
+            
             output.Add(buffer);
         }
 
@@ -67,25 +70,26 @@ namespace MineLW.Networking.Handlers
                 
                 msg.Retain();
                 output.Add(msg);
+                return;
             }
-            else
-            {
-                var compressedData = msg.ToArray(out var offset, out var length);
-                
-                _decompressCodec.InputBuffer = compressedData;
-                _decompressCodec.NextIn = offset;
-                _decompressCodec.AvailableBytesIn = length;
+            
+            if (uncompressedSize < _compressionThreshold)
+                throw new DecoderException("Badly compressed message (uncompressed size: " + uncompressedSize + ')');
+            if (uncompressedSize > 1 << 21)
+                throw new DecoderException("Badly compressed message (size over protocol limit: " + uncompressedSize + ')');
 
-                var data = new byte[uncompressedSize];
-                _decompressCodec.OutputBuffer = data;
-                _decompressCodec.NextOut = 0;
-                _decompressCodec.AvailableBytesOut = uncompressedSize;
-                
-                _decompressCodec.Inflate(FlushType.Finish);
-                _decompressCodec.EndInflate();
-                
-                output.Add(Unpooled.WrappedBuffer(data));
-            }
+            var inputBuffer = msg.ToArray(out var offset, out var count);
+            _inflater.SetInput(inputBuffer, offset, count);
+            
+            var outputBuffer = new byte[uncompressedSize];
+            var processedBytes = _inflater.Inflate(outputBuffer);
+            
+            _inflater.Reset();
+            
+            if(uncompressedSize != processedBytes)
+                throw new DecoderException("Badly compressed message");
+            
+            output.Add(Unpooled.WrappedBuffer(outputBuffer));
         }
     }
 }
