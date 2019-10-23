e902c64a339a680cb3c241447b92cb10b5e695f4
TheWhoosher <18100602+TheWhoosher@users.noreply.github.com>
2019-10-23 15:11:37 +0200

    Added ClientController. This class is responsable for all the client inputs (received messages)

---
+++ MineLW.API/Client/IClientController.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using MineLW.API.Entities.Events;
+using MineLW.API.Entities.Living.Player;
+
+namespace MineLW.API.Client
+{
+    public interface IClientController
+    {
+        event EventHandler<EntityPositionChangedEventArgs> PositionChanged;
+
+        void Init(IEntityPlayer player);
+    }
+}
