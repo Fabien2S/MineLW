3b2eac4fb2b67eca998e7068d1d99944c5a78fe8
TheWhoosher <18100602+TheWhoosher@users.noreply.github.com>
2019-10-24 17:55:18 +0200

    Added Minecraft.Entities constant

---
+++ MineLW.API/MinecraftEntities.cs
@@ -0,0 +1,12 @@
+﻿using MineLW.API.Utils;
+
+namespace MineLW.API
+{
+    public static partial class Minecraft
+    {
+        public static class Entities
+        {
+            public static readonly Identifier Player = CreateIdentifier("player");
+        }
+    }
+}
