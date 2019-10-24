5979c0e64f79cc4dc394aebeb191316e4d36b505
TheWhoosher <18100602+TheWhoosher@users.noreply.github.com>
2019-10-24 13:34:37 +0200

    Added EnumSerializer

---
+++ MineLW.API/Commands/Serializers/EnumSerializer.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.ComponentModel;
+using MineLW.API.Commands.Exceptions;
+using MineLW.API.IO;
+
+namespace MineLW.API.Commands.Serializers
+{
+    public class EnumSerializer : ArgumentSerializer<Enum>
+    {
+        public override string Serialize(Enum value)
+        {
+            var type = value.GetType();
+            var name = Enum.GetName(type, value);
+            if (name == null)
+                throw new InvalidEnumArgumentException("Invalid enum " + value);
+            return name.ToLowerInvariant();
+        }
+
+        public override Enum Deserialize(Type type, StringReader reader)
+        {
+            var enumValue = reader.ReadString();
+            if (Enum.TryParse(type, enumValue, true, out var e))
+                return (Enum) e;
+            throw new CommandParseException("Invalid enum value (" + enumValue + ')');
+        }
+    }
+}
