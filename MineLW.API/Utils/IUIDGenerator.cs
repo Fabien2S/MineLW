061fc28866d8d6dbc0b2d52da19a5819ae480e1c
TheWhoosher <18100602+TheWhoosher@users.noreply.github.com>
2019-10-25 15:36:12 +0200

    Added Unique ID Generator interface

---
+++ MineLW.API/Utils/IUIDGenerator.cs
@@ -0,0 +1,11 @@
+﻿namespace MineLW.API.Utils
+{
+    public interface IUidGenerator
+    {
+        /// <summary>
+        /// Generate a unique id
+        /// </summary>
+        /// <returns>a unique id</returns>
+        int GenerateUid();
+    }
+}
