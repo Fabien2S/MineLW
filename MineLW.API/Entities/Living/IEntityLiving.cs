d62968f7ce8e7738a3a473eb56c8377eeab5a70f
TheWhoosher <18100602+TheWhoosher@users.noreply.github.com>
2019-10-29 15:15:39 +0100

    Added HeadAngle support

--- MineLW.API/Entities/Living/IEntityLiving.cs
+++ MineLW.API/Entities/Living/IEntityLiving.cs
@@ -1,6 +1,13 @@
-﻿namespace MineLW.API.Entities.Living
+﻿using System;
+using MineLW.API.Entities.Events;
+
+namespace MineLW.API.Entities.Living
 {
     public interface IEntityLiving : IEntity
     {
+        event EventHandler<EntityRotationChangingEventArgs> HeadAngleChanging;
+        event EventHandler<EntityRotationChangedEventArgs> HeadAngleChanged;
+        
+        float HeadAngle { get; set; }
     }
 }
--- MineLW.Entities/Living/EntityLiving.cs
+++ MineLW.Entities/Living/EntityLiving.cs
@@ -1,10 +1,38 @@
 ﻿using System;
+using MineLW.API.Entities.Events;
 using MineLW.API.Entities.Living;
+using MineLW.API.Math;
 
 namespace MineLW.Entities.Living
 {
     public abstract class EntityLiving : Entity, IEntityLiving
     {
+        public event EventHandler<EntityRotationChangingEventArgs> HeadAngleChanging;
+        public event EventHandler<EntityRotationChangedEventArgs> HeadAngleChanged;
+
+        public float HeadAngle
+        {
+            get => _headAngle;
+            set
+            {
+                var entityRotation = Rotation;
+
+                var previousRotation = new Rotation(_headAngle, entityRotation.Pitch);
+                var currentRotation = new Rotation(value, entityRotation.Pitch);
+                var rotationChangingEventArgs = new EntityRotationChangingEventArgs(previousRotation, currentRotation);
+                HeadAngleChanging?.Invoke(this, rotationChangingEventArgs);
+                if (rotationChangingEventArgs.Cancel)
+                    return;
+
+                _headAngle = value;
+
+                var rotationChangedEventArgs = new EntityRotationChangedEventArgs(previousRotation, currentRotation);
+                HeadAngleChanged?.Invoke(this, rotationChangedEventArgs);
+            }
+        }
+
+        private float _headAngle;
+
         protected EntityLiving(int id, Guid uuid) : base(id, uuid)
         {
         }
