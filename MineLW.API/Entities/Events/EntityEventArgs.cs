095327c3e90c3e142d5424d20edc726ff767141f
TheWhoosher <18100602+TheWhoosher@users.noreply.github.com>
2019-10-29 15:10:51 +0100

    Added EntityEventArgs

---
+++ MineLW.API/Entities/Events/EntityEventArgs.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace MineLW.API.Entities.Events
+{
+    public class EntityEventArgs : EventArgs
+    {
+        public readonly IEntity Entity;
+
+        public EntityEventArgs(IEntity entity)
+        {
+            Entity = entity;
+        }
+    }
+}
